2515aa3bfc3be9a499dfc0cebeb864e89f986588
DH3155 <darian.elbert@gmail.com>
2023-10-15 13:59:01 +0700

    Plotting for OS and Brand

--- main.R
+++ main.R
@@ -1,4 +1,19 @@
-data = read.csv("ndtv_data_final.csv")
-data2 = read.csv("phones_data.csv")
+data = read.csv("phones_data.csv")
 data = na.omit(data)
-data2 = na.omit(data2)
+
+
+
+OS <- table(data$os)
+barplot(OS)
+
+brand <- table(data$brand_name)
+sum <- brand
+
+
+brandName <- names(brand)
+
+sum(brand)
+
+piepercent <- round(100*brand / sum(brand), 2)
+piepercent
+pie(piepercent, label = brandName, cex = 0.6, col = rainbow(length(piepercent)))
