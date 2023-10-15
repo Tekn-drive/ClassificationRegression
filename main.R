a5d0a25900b9c4ba232d839e06432d76de3f1b3d
Dave Sebastian <darksebastianwijono@gmail.com>
2023-10-15 14:12:48 +0700

    currency rate

--- main.R
+++ main.R
@@ -17,3 +17,9 @@ sum(brand)
 piepercent <- round(100*brand / sum(brand), 2)
 piepercent
 pie(piepercent, label = brandName, cex = 0.6, col = rainbow(length(piepercent)))
+
+#change currency to USD from UAH (avg of 2020, 1 USD = 26.9735 UAH)
+data$best_price = data$best_price / 26.9735
+data$lowest_price = data$lowest_price / 26.9735
+data$highest_price = data$highest_price / 26.9735
+
